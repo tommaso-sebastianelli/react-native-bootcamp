3805c7e5c3b94ee0ae80aafe4286dc37242e6482
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.office.local>
2020-01-31 17:08:30 +0100

    login page

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import {View} from 'react-native';
+import React, { useState } from 'react'
+import { Button } from 'react-native';
+import { View, TextInput } from 'react-native';
+import { connect } from 'react-redux';
+import { authRequestStart } from '../../redux/auth/actions';
 
-function Login(){
+import styles from './style';
+
+
+
+function Login({ doLogin }) {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onChangeUser = text => {
+        setUsername(text);
+    }
+
+    const onChangePassword = text => {
+        setPassword(text);
+    }
+
     return (
-        <View></View>
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                onChangeText={text => onChangeUser(text)}
+                value={username}
+            />
+            <TextInput
+                style={styles.input}
+                secureTextEntry
+                onChangeText={text => onChangePassword(text)}
+                value={password}
+            />
+            <Button title="Login" onPress={() => doLogin()}></Button>
+        </View>
     )
 }
 
-export default Login
+const mapStateToProps = (state) => ({
+
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    doLogin: () => dispatch(authRequestStart('dummyUser', 'dummyPwd'))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
---
+++ src/containers/Login/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        margin: 32
+    },
+    input: {
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
+        marginBottom: 16
+    }
+});
