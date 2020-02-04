968f9d5eb9c5c7c2e716d56f09b4b3ee93ca67b0
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.office.local>
2020-02-04 11:08:25 +0100

    created missing actions, edited sagas

--- src/redux/auth/sagas.js
+++ src/redux/auth/sagas.js
@@ -1,38 +1,31 @@
-import { AUTH_OFF, AUTH_REQUEST_FAILURE, AUTH_REQUEST_START, AUTH_REQUEST_SUCCESS } from './types'
-import {
-  all,
-  call,
-  delay,
-  put,
-  select,
-  take,
-} from 'redux-saga/effects'
-
-import { credentialSelector } from './selectors';
+import { all, call, delay, put, select, take } from 'redux-saga/effects';
 import NavigationService from '../../../NavigatorService';
+import { credentialSelector } from './selectors';
+import { AUTH_OFF, AUTH_REQUEST_FAILURE, AUTH_REQUEST_START, AUTH_REQUEST_SUCCESS } from './types';
+import { signInSuccess, signInFailure } from './actions';
 
 const mockAuthorize = () => new Promise((resolve) => resolve({ username: 'dummy.usermail.com', token: '6fbu3r93urGVIWd3DG$)/Y)/ygdtd3d' }));
 
 function* logout() {
   while (true) {
     yield take(AUTH_OFF);
-    yield call(() => NavigationService.navigate('AuthLoading'));
+    yield NavigationService.navigate('AuthLoading');
   }
 }
 
 function* login() {
   while (true) {
     yield take(AUTH_REQUEST_START);
-    yield call(() => NavigationService.navigate('AuthLoading'));
+    yield NavigationService.navigate('AuthLoading');
     try {
       const { state_username, state_password } = yield select(credentialSelector);
       const { username, token } = yield call(mockAuthorize, state_username, state_password);
       console.log(username, token);
       yield delay(2000);
-      yield put({ type: AUTH_REQUEST_SUCCESS, payload: { username: username, authToken: token, loading: false } });
+      yield put(signInSuccess({ username: username, authToken: token }));
     } catch (err) {
       console.error(err);
-      yield put({ type: AUTH_REQUEST_FAILURE, payload: { error: err, loading: false } });
+      yield put(signInFailure({ error: err }));
     }
   }
 }
